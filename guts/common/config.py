1e178133a165f7797ea884ef5ac0b3d36db8e367
Bharat Kumar <bharat@aptira.com>
2015-12-10 18:59:57 +0530

    GUTS: Packaging Tools
    This patch contains Python packaging tools required to setup guts
    project.
    
    This patch also contains testing tools required for unit tests
    and to check Python PEP8 standards.

---
+++ guts/common/config.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2015 Aptira Pty Ltd.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Command-line flag library.
+
+Emulates gflags by wrapping cfg.ConfigOpts.
+
+The idea is to move fully to cfg eventually, and this wrapper is a
+stepping stone.
+
+"""
+
+from oslo_config import cfg
+
+
+CONF = cfg.CONF
+
+global_opts = [
+    cfg.StrOpt('rootwrap_config',
+               default='/etc/guts/rootwrap.conf',
+               help='Path to the rootwrap configuration file to use for '
+                    'running commands as root'),
+]
+
+CONF.register_opts(global_opts)
---
+++ guts/utils.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2015 Aptira Pty Ltd.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Utilities and helper functions."""
+
+from oslo_concurrency import processutils
+from oslo_config import cfg
+
+CONF = cfg.CONF
+
+
+def get_root_helper():
+    return 'sudo guts-rootwrap %s' % CONF.rootwrap_config
+
+
+def execute(*cmd, **kwargs):
+    """Convenience wrapper around oslo's execute() method."""
+    if 'run_as_root' in kwargs and 'root_helper' not in kwargs:
+        kwargs['root_helper'] = get_root_helper()
+    return processutils.execute(*cmd, **kwargs)
