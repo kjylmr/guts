1751860361a1b528cf41cb342c6b90f7ba3d43c1
bala <bala@aptira.com>
2016-06-09 05:12:53 +0000

    "Removing Source Hypervisor configuration."

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -75,21 +75,6 @@ function configure_guts {
     iniset $GUTS_CONF DEFAULT enabled_source_hypervisors source_openstack
     iniset $GUTS_CONF DEFAULT enabled_destination_hypervisors destination_openstack
 
-    # Configure OpenStack source driver
-    iniset $GUTS_CONF source_openstack source_driver guts.migration.drivers.sources.openstack.OpenStackSourceDriver
-    iniset $GUTS_CONF source_openstack capabilities instance,volume,network
-    iniset $GUTS_CONF source_openstack auth_url
-    iniset $GUTS_CONF source_openstack username
-    iniset $GUTS_CONF source_openstack password
-    iniset $GUTS_CONF source_openstack tenant_name
-
-    # Configure VMware source driver
-    iniset $GUTS_CONF source_vmware source_driver guts.migration.drivers.sources.vsphere.VSphereSourceDriver
-    iniset $GUTS_CONF source_vmware capabilities instance
-    iniset $GUTS_CONF source_vmware vsphere_host
-    iniset $GUTS_CONF source_vmware vsphere_username
-    iniset $GUTS_CONF source_vmware vsphere_password
-
     # configure rpc backend
     configure_guts_rpc_backend
 
